9832f0ec185413ed980628a783d11c889fba300d
NailykSturm <47662089+NailykSturm@users.noreply.github.com>
2022-09-20 20:55:53 +0200

    Send PNJ image wen ask
    - Change location of resources folder (better for acess to it)
    - Return PNJ image when it was asked

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import fs from "fs";
 import * as path from 'path';
+const RESOURCES_PATH = './resources/';
 
 function readJson(pathFile: string) {
     const encoding = 'utf8';
@@ -19,34 +20,48 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req: any, res: any) => {
-    res.send(readJson('../resources/json/1.json'));
+    res.send(readJson(`${RESOURCES_PATH}json/1.json`));
 
 });
 
 app.post("/", (req: any, res: any) => {
-    console.log(req.body);
     const nexts = req.body.next;
-    console.log(nexts);
-    const next = nexts[getRndInteger(0, nexts.length)];
-    console.log(next);
-    try {
-        const jsonres = readJson(`../resources/json/${next}.json`);
-        console.log(jsonres);
-        res.send(jsonres);
-    } catch (e: any) {
-        var jsonres = readJson(`../resources/json/${req.body.id}.json`);
-        if(e.code == "ENOENT"){
-            console.log("Attention, on essaie d'accéder à un fichier inconnu !");
-            jsonres.error = "Une erreure est survenue => la suite de la quête n'est pas encore implémenter. Veuillez réessayer";
+    if (nexts != undefined) {
+        const next = nexts[getRndInteger(0, nexts.length)];
+        try {
+            const jsonres = readJson(`${RESOURCES_PATH}json/${next}.json`);
             res.send(jsonres);
-        }else {
-            console.log(e);
-            jsonres.error = "Une erreure est survenue, on y travaille, désolé. Veuillez réessayer";
-            res.send(jsonres);
+        } catch (e: any) {
+            var jsonres = readJson(`${RESOURCES_PATH}json/${req.body.id}.json`);
+            if (e.code == "ENOENT") {
+                console.log("Attention, on essaie d'accéder à un fichier inconnu !");
+                jsonres.error = "Une erreure est survenue => la suite de la quête n'est pas encore implémenter. Veuillez réessayer";
+                res.send(jsonres);
+            } else {
+                console.log(e);
+                jsonres.error = "Une erreure est survenue, on y travaille, désolé. Veuillez réessayer";
+                res.send(jsonres);
+            }
         }
+    } else {
+        var json = JSON.parse("{}");
+        json.error = "Rien n'a été envoyé! Comment suis-je censé répondre?";
+        res.send(json);
     }
 });
 
-app.all('*')
+app.post("/pnj", (req: any, res: any) => {
+    const pnjasked = req.body.pnjImg;
+    console.log("pnjasked = " + pnjasked);
+    if (pnjasked != undefined) {
+        const path = `${RESOURCES_PATH.slice(1)}${pnjasked}`;
+        console.log("full path = " + __dirname + path);
+        res.sendFile(__dirname + path);
+    } else {
+        var json = JSON.parse("{}");
+        json.error = "Rien n'a été envoyé! Comment suis-je censé répondre?";
+        res.send(json);
+    }
+});
 
 app.listen(3000, () => { console.log("Server running on port 3000"); });
