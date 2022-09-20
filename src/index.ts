bca2527e7ab05128e2ecbe14593432b32e409385
NailykSturm <47662089+NailykSturm@users.noreply.github.com>
2022-09-20 20:10:14 +0200

    Choose next question
    - API choose the next question randomely in a list of questions
    - Manage error if a question choosen is not define

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ function readJson(pathFile: string) {
     return JSON.parse(file);
 }
 
+function getRndInteger(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
 var express = require("express");
 var app = express();
 
@@ -15,12 +19,32 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req: any, res: any) => {
-    res.send(readJson('../resources/template.json'));
+    res.send(readJson('../resources/json/1.json'));
+
 });
 
 app.post("/", (req: any, res: any) => {
     console.log(req.body);
-    res.send(readJson('../resources/template.json'));
+    const nexts = req.body.next;
+    console.log(nexts);
+    const next = nexts[getRndInteger(0, nexts.length)];
+    console.log(next);
+    try {
+        const jsonres = readJson(`../resources/json/${next}.json`);
+        console.log(jsonres);
+        res.send(jsonres);
+    } catch (e: any) {
+        var jsonres = readJson(`../resources/json/${req.body.id}.json`);
+        if(e.code == "ENOENT"){
+            console.log("Attention, on essaie d'accéder à un fichier inconnu !");
+            jsonres.error = "Une erreure est survenue => la suite de la quête n'est pas encore implémenter. Veuillez réessayer";
+            res.send(jsonres);
+        }else {
+            console.log(e);
+            jsonres.error = "Une erreure est survenue, on y travaille, désolé. Veuillez réessayer";
+            res.send(jsonres);
+        }
+    }
 });
 
 app.all('*')
